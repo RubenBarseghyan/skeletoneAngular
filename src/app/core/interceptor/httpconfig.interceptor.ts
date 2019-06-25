d31ee94709b0264004f3f075a2daf0b490ed27a0
Ruben <rubenbarseghyan88@gmail.com>
2019-06-25 22:31:41 +0400

    interceptor

--- src/app/core/interceptor/httpconfig.interceptor.ts
+++ src/app/core/interceptor/httpconfig.interceptor.ts
@@ -1,1 +1,26 @@
-// lmlml
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+@Injectable()
+export class HttpConfigInterceptor implements HttpInterceptor {
+  constructor() {}
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.getToken()) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `${this.getToken()}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+  public getToken(): boolean {
+    return localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')).token : false;
+  }
+}
