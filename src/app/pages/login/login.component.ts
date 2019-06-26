3c0da779288a5edd84857ea06b197a57bfb7260b
Ruben <rubenbarseghyan88@gmail.com>
2019-06-26 11:59:01 +0400

    error message from back and FormGroup

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {RepositoryService} from '../../core/services/repository.service';
 import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,10 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   public userFormLogin: FormGroup;
-  public errorMes: string;
+  public  formControlEmailMsg = 'Not Valid Email';
+  public  formControlPassMsg = 'password length must be 4-15 symbols';
+  public emailErrMsgFromBackend: string;
+  public passErrMsgFromBackend: string;
   public isLoad = false;
 
   constructor(private  http: RepositoryService, private router: Router) { }
@@ -18,15 +22,15 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
     this.userFormLogin = new FormGroup({
       userEmail: new FormControl('', [ Validators.required, Validators.pattern('[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}')]),
-      userPassword: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(10)])
+      userPassword: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(15)])
     });
   }
   public onSubmit() {
-    const url: string = 'api/user/login';
+    const url = 'api/user/login';
     const body  = this.userFormLogin.getRawValue();
     this.http.create(url, body).subscribe((res: any) => {
       console.log('login');
-      if(res.token){
+      if (res.token) {
         localStorage.setItem('userInfo', JSON.stringify(res));
       }
       if (res.role === 'user') {
@@ -34,6 +38,13 @@ export class LoginComponent implements OnInit {
       } else {
         this.router.navigate(['admin']); }
 
-    }, error => this.errorMes = error.message);
+    }, (error) => {
+      if (error.status === '404') {
+        this.emailErrMsgFromBackend = error.error.message;
+      } else {
+        this.passErrMsgFromBackend = error.error.message;
+      }
+    });
+    // error message comes from backend we only draw it
   }
 }
