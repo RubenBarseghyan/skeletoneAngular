ae7d23d0b9e5a60ee6299fdcee4ba9718175f3d8
Ruben <rubenbarseghyan88@gmail.com>
2019-06-27 14:10:58 +0400

    header component a links functionality

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
 
+  }
+
+  public logOut(): void {
+    localStorage.removeItem('userInfo');
+    this.router.navigate(['login']);
   }
 
 }
